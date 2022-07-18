84f6ed1d8a04a505f5efeb0f6f7cbc682be4e4f9
РоманСапун <rsapun@ukr.net>
2022-07-18 21:15:05 +0300

    decoder implemented

--- CryptoAnaliser/src/main/java/ru/javarush/cryptoanaliser/sapun/commands/Decoder.java
+++ CryptoAnaliser/src/main/java/ru/javarush/cryptoanaliser/sapun/commands/Decoder.java
@@ -7,12 +7,48 @@ import ru.javarush.cryptoanaliser.sapun.util.PathFinder;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
+
+import static ru.javarush.cryptoanaliser.sapun.constans.Strings.ALPHABET;
+import static ru.javarush.cryptoanaliser.sapun.entity.ResultCode.OK;
 
 public class Decoder implements Action{
     @Override
     public Result execute(String[] parameters) {
-        //TODO need dev logic decrypt
-        return null;
+        String encryptedFile = parameters[0];
+        String outFile = parameters[1];
+        Path inputPath = Path.of(PathFinder.getRoot() + encryptedFile);
+        try {
+            List<String> strings = Files.readAllLines(inputPath);
+            List<String> totalResult = new ArrayList<>();
+            for (String string : strings) {
+                char[] chars = string.toCharArray();
+                char[] result = new char[chars.length];
+                for (int i = 0; i < chars.length; i++) {
+                    result[i] = decodeChar(chars[i]);
+                }
+                totalResult.add(new String(result));
+            }
+
+            Path outPath = Path.of(PathFinder.getRoot() + outFile);
+            Files.write(outPath, totalResult);
+        } catch (IOException e) {
+            throw new ApplicationException("IO error", e);
+        }
+
+        return new Result(OK, "decode all from file: " + inputPath);
+    }
+    private char decodeChar(char element) {
+        if (!ALPHABET.contains(String.valueOf(element))) {
+            return element;
+        }
+
+        int index = ALPHABET.indexOf(element);
+        int neededIndex = (index - KEY);
+        if (neededIndex < 0){
+            neededIndex += ALPHABET.length();
+        }
+        return ALPHABET.charAt(neededIndex);
     }
 }
